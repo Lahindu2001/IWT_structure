8aa90bf22baf15a5b98a6d2c2b3cd5242171dbcb
lahindu ishan <[lahindu2001ishan@gmail.com]>
2024-09-28 02:05:59 +0530

    create plane file with heder and foter

---
+++ Famillyinone.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="src/asserts/css/coverage.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>Document</title>
+</head>
+
+<body>
+  <nav>
+    <div class="navlinks">
+      <img src="src/asserts/images/logo.png">
+      <a href="#">Home</a>
+      <div class="plan-section">
+        <button id="planBtn">Insurance Plans</button>
+        <div class="plan-category">
+          <a href="#">Emergency Coverage</a>
+          <a href="#">Complete Coverage</a>
+          <a href="#">Family all-in-one</a>
+          <a href="#">Elder Citizense</a>
+        </div>
+      </div>
+      <a href="#">About Us</a>
+      <a href="#">Contact Us</a>
+      <div class="sub-log-Btn">
+        <button id="signinBtn">Sign In</button>
+        <button id="loginBtn">Log In</button>
+      </div>
+    </div>
+  </nav>
+
+  <div class="banner">
+    <img src="src/asserts/images/1.jpg" alt="">
+    <h1>FAMILY ALL IN ONE PLAN</h1>
+  </div>
+
+
+  <h1 class="topicForPlans">We Are Covering...</h1>
+  <div class="plansType">
+
+    <div class="cont">
+      <h2>Financial Security for Loved Ones</h2>
+      <img src="src/asserts/images/f1.jpg" alt="">
+      <p>A family life insurance plan ensures that your family is financially secure in case of the policyholder’s untimely death.  <b>See more...</b></p>
+    </div>
+
+    <div class="cont">
+      <h2>Affordable Premiums</h2>
+      <img src="src/asserts/images/f4.jpg" alt="">
+      <p>Family plans often come with the advantage of being more cost-effective, offering coverage for multiple members under a single policy . <b>See more...</b>
+      </p>
+    </div>
+
+    <div class="cont">
+      <h2>Comprehensive Coverage</h2>
+      <img src="src/asserts/images/f3.jpg" alt="">
+      <p>These plans can provide comprehensive coverage for all family members, including add-ons like critical illness . <b>See more...</b></p>
+    </div>
+
+    <div class="cont">
+      <h2>Peace of Mind</h2>
+      <img src="src/asserts/images/f2.jpg" alt="">
+      <p>Knowing that your family is covered brings peace of mind. A family plan ensures that in case of unforeseen circumstances, . <b>See more...</b></p>
+    </div>
+  </div>
+
+
+  <div class="mainCont">
+
+    <div class="contInner">
+      <div><img src="src/asserts/images/f1.jpg" alt=""></div>
+      <div class="description">
+        <h1>Financial Security for Loved Ones</h1>
+        <p>A family life insurance plan ensures that your family is financially secure in case of the policyholder’s untimely death. The lump sum payout or regular income stream can cover expenses such as education, mortgage, and daily living costs.</p>
+      </div>
+    </div>
+
+    <div class="contInner">
+      <div class="description">
+        <h1>Affordable Premiums</h1>
+        <p>Family plans often come with the advantage of being more cost-effective, offering coverage for multiple members under a single policy. This makes it easier to manage and more affordable compared to individual policies.</p>
+      </div>
+      <div><img src="src/asserts/images/f4.jpg" alt=""></div>
+    </div>
+
+    <div class="contInner">
+      <div><img src="src/asserts/images/f3.jpg" alt=""></div>
+      <div class="description">
+        <h1>Comprehensive Coverage</h1>
+        <p>These plans can provide comprehensive coverage for all family members, including add-ons like critical illness, accidental death, or disability coverage, ensuring that your family is protected against a variety of risks..</p>
+      </div>
+    </div>
+
+    <div class="contInner">
+      <div class="description">
+        <h1>Peace of Mind</h1>
+        <p>Knowing that your family is covered brings peace of mind. A family plan ensures that in case of unforeseen circumstances, your loved ones will have financial support, which can be vital in difficult times.</p>
+      </div>
+      <div><img src="src/asserts/images/f2.jpg" alt=""></div>
+    </div>
+  </div>
+  </div>
+
+  <footer>
+    <div class="leftImg">
+      <img src="src/asserts/images/logo.png" alt="">
+    </div>
+    <div class="rightCont">
+      <div class="footNavLinks">
+        <a href="#">Home</a>
+        <a href="#">Insurance Plans</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+      </div>
+      <p>Life insurance supports your family with money if you pass away. How Insurance Works You pay a small amount of
+        money regularly, called a premium, to the insurance company. Many people do this, and the company collects a lot
+        of money. Most of the time, nothing bad happens, so the company doesn't need to use all the money.</p>
+      <div class="social">
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-square-instagram"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-telegram"></i>
+      </div>
+
+      <div class="lastcont">
+        <p>Copyrigh © 2024 Website All rights</p>
+        <p><u>Team & Conditionas | Privacy Policy</u></p>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
---
+++ emargency.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="src/asserts/css/coverage.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>Document</title>
+</head>
+
+<body>
+  <nav>
+    <div class="navlinks">
+      <img src="src/asserts/images/logo.png">
+      <a href="#">Home</a>
+      <div class="plan-section">
+        <button id="planBtn">Insurance Plans</button>
+        <div class="plan-category">
+          <a href="#">Emergency Coverage</a>
+          <a href="#">Complete Coverage</a>
+          <a href="#">Family all-in-one</a>
+          <a href="#">Elder Citizense</a>
+        </div>
+      </div>
+      <a href="#">About Us</a>
+      <a href="#">Contact Us</a>
+      <div class="sub-log-Btn">
+        <button id="signinBtn">Sign In</button>
+        <button id="loginBtn">Log In</button>
+      </div>
+    </div>
+  </nav>
+
+  <div class="banner">
+    <img src="src/asserts/images/emg.jpg" alt="">
+    <h1>Emergency Coverage</h1>
+  </div>
+
+
+  <h1 class="topicForPlans">We Are Covering...</h1>
+  <div class="plansType">
+
+    <div class="cont">
+      <h2>No Waiting Period for Emergency Benefits</h2>
+      <img src="src/asserts/images/e4.jpg" alt="">
+      <p>Unlike traditional life insurance policies that might have a waiting period before certain benefits kick in, emergency coverage for elders typically offers immediate protection. <b>See more...</b></p>
+    </div>
+
+    <div class="cont">
+      <h2>Accidental Death or Disability Coverage</h2>
+      <img src="src/asserts/images/e3.jpg" alt="">
+      <p>This type of plan often includes special provisions for accidental death or disability, providing additional financial support if the insured passes away,<b>See more...</b>
+      </p>
+    </div>
+
+    <div class="cont">
+      <h2>Access to Emergency Medical Services</h2>
+      <img src="src/asserts/images/e2.jpg" alt="">
+      <p>Many elderly emergency plans come with add-on benefits such as access to emergency medical services, <b>See more...</b></p>
+    </div>
+
+    <div class="cont">
+      <h2>Immediate Financial Support in Emergencies</h2>
+      <img src="src/asserts/images/e1.jpg" alt="">
+      <p>An elderly emergency coverage plan ensures rapid financial assistance in critical situations such as accidents, sudden hospitalization,<b>See more...</b></p>
+    </div>
+  </div>
+
+
+  <div class="mainCont">
+
+    <div class="contInner">
+      <div><img src="src/asserts/images/e4.jpg" alt=""></div>
+      <div class="description">
+        <h1>No Waiting Period for Emergency Benefits</h1>
+        <p>Unlike traditional life insurance policies that might have a waiting period before certain benefits kick in, emergency coverage for elders typically offers immediate protection.
+           This means that seniors are covered from day one for emergency medical situations, ensuring timely financial support when it’s most needed.
+
+          This type of plan ensures that elderly individuals are well-protected against unforeseen medical emergencies, allowing them and their families to navigate crisis situations with financial security.</p>
+      </div>
+    </div>
+
+    <div class="contInner">
+      <div class="description">
+        <h1>Accidental Death or Disability Coverage</h1>
+        <p>This type of plan often includes special provisions for accidental death or disability, 
+          providing additional financial support if the insured passes away or becomes disabled due to an accident.</p>
+      </div>
+      <div><img src="src/asserts/images/e3.jpg" alt=""></div>
+    </div>
+
+    <div class="contInner">
+      <div><img src="src/asserts/images/e2.jpg" alt=""></div>
+      <div class="description">
+        <h1>Access to Emergency Medical Services</h1>
+        <p>Many elderly emergency plans come with add-on benefits such as access to emergency medical services, ambulance cover, or even telemedicine services.
+           This ensures that seniors can quickly get the medical attention they need without worrying about upfront costs or logistical hurdles.</p>
+      </div>
+    </div>
+
+    <div class="contInner">
+      <div class="description">
+        <h1>Immediate Financial Support in Emergencies</h1>
+        <p>An elderly emergency coverage plan ensures rapid financial assistance in critical situations such as accidents, sudden hospitalization, or emergency medical procedures. The plan provides immediate payouts or 
+          reimbursement to cover medical bills, reducing the financial burden on family members during urgent situations.. </p>
+      </div>
+      <div><img src="src/asserts/images/e1.jpg" alt=""></div>
+    </div>
+  </div>
+  </div>
+
+  <footer>
+    <div class="leftImg">
+      <img src="src/asserts/images/logo.png" alt="">
+    </div>
+    <div class="rightCont">
+      <div class="footNavLinks">
+        <a href="#">Home</a>
+        <a href="#">Insurance Plans</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+      </div>
+      <p>Life insurance supports your family with money if you pass away. How Insurance Works You pay a small amount of
+        money regularly, called a premium, to the insurance company. Many people do this, and the company collects a lot
+        of money. Most of the time, nothing bad happens, so the company doesn't need to use all the money.</p>
+      <div class="social">
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-square-instagram"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-telegram"></i>
+      </div>
+
+      <div class="lastcont">
+        <p>Copyrigh © 2024 Website All rights</p>
+        <p><u>Team & Conditionas | Privacy Policy</u></p>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
